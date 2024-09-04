b1fa4115f79cf59789e13a2a430d96f9c67d63c1
Akshey D <aksheyd@umich.edu>
2024-09-04 18:07:08 -0400

    Initial Commit

---
+++ FoodFinder/FoodFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFinderApp.swift
+//  FoodFinder
+//
+//  Created by Akshey Deokule on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
