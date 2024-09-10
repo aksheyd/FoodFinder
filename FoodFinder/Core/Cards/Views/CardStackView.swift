6d85958d3a53cf0f8cae2bce5f9bd079477da6bc
Akshey D <aksheyd@umich.edu>
2024-09-10 15:09:49 -0400

    swipe cards off screen

---
+++ FoodFinder/Core/Cards/Views/CardStackView.swift
@@ -0,0 +1,18 @@
+//
+//  CardStackView.swift
+//  FoodFinder
+//
+//  Created by Akshey Deokule on 9/10/24.
+//
+
+import SwiftUI
+
+struct CardStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardStackView()
+}
